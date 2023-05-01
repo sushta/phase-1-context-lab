ecbe30969397437f514182622cae60ee21b1d4a1
sustapakhreen <sushta48@gmail.com>
2023-04-30 22:28:17 -0500

    completion of context lab

--- index.js
+++ index.js
@@ -1,4 +1,81 @@
-/* Your Code Here */
+function createEmployeeRecord(arr) {
+    let employee = {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+    return employee;
+}
+
+// createEmployeeRecords
+//Argument(s)
+//Array of Arrays
+//Returns
+//Array of Objects
+//Behavior
+//Converts each nested Array into an employee record using createEmployeeRecord and accumulates it to a new Array
+
+function createEmployeeRecords(arr) {
+    return arr.map(employeeArr => createEmployeeRecord(employeeArr));
+}
+function createTimeInEvent( dateStamp) {
+    let [date, hour] = dateStamp.split(' ');
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date: date
+    });
+
+    return this;
+}
+
+function createTimeOutEvent( dateStamp) {
+    let [date, hour] = dateStamp.split(' ');
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date: date
+    });
+
+    return this;
+}
+
+
+
+function hoursWorkedOnDate(date) {
+    const timeInEvents = this.timeInEvents.find(e => e.date === date);
+    const timeOutEvents = this.timeOutEvents.find(e => e.date === date);
+  
+    const hoursWorked = (timeOutEvents.hour - timeInEvents.hour) / 100;
+  
+    return hoursWorked;
+  }
+  
+  function wagesEarnedOnDate(date) {
+    const hoursWorked = hoursWorkedOnDate.call(this, date);
+    const payRate = this.payPerHour;
+    const wagesEarned = hoursWorked * payRate;
+    return wagesEarned;
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(record => record.firstName === firstName);
+}
+
+function calculatePayroll(employeeRecords) {
+    const totalPayroll = employeeRecords.reduce((acc, record) => {
+        return acc + allWagesFor.call(record);
+    }, 0);
+
+    return totalPayroll;
+}
+
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
